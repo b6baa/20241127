c95cf8b84d4d34b37c422aaa71babdcff98c32ba
b6baa <tina200205106@gmail.com>
2024-12-02 13:13:51 +0800

    Update 3_📍_Marker_Cluster.py

--- pages/3_📍_Marker_Cluster.py
+++ pages/3_📍_Marker_Cluster.py
@@ -3,9 +3,6 @@ import leafmap.foliumap as leafmap
 import geopandas as gpd
 import requests
 
-url = "https://data.tainan.gov.tw/dataset/bf7eb079-8b9f-4248-a4a2-877962258763/resource/9072b1a5-7657-406e-8278-a8073167ddb0/download/f0006.json"
-response = requests.get(url)
-
 st.set_page_config(layout="wide")
 
 markdown = """
@@ -22,21 +19,16 @@ st.title("Marker Cluster")
 
 with st.expander("See source code"):
     with st.echo():
-        #gdf = gpd.read_file("https://github.com/b6baa/20241127ex2/raw/3edcd55cd41bc1238004abe6d58e0ecf6fefac32/%E5%8F%B0%E4%B8%AD%E5%B8%82%E5%8D%80%E7%95%8C_TWD97-1.shp")
-        #regions_geojson = gdf.to_json()
-
         m = leafmap.Map(center=[40, -100], zoom=4)
-        #bus_stop = "https://github.com/b6baa/20241127ex2/raw/3edcd55cd41bc1238004abe6d58e0ecf6fefac32/%E8%87%BA%E4%B8%AD%E5%B8%82%E5%B8%82%E5%8D%80%E5%85%AC%E8%BB%8A%E7%AB%99%E7%89%8C%E8%B3%87%E6%96%99.csv"
+        bus_stop = "https://github.com/b6baa/20241127ex2/raw/3edcd55cd41bc1238004abe6d58e0ecf6fefac32/%E8%87%BA%E4%B8%AD%E5%B8%82%E5%B8%82%E5%8D%80%E5%85%AC%E8%BB%8A%E7%AB%99%E7%89%8C%E8%B3%87%E6%96%99.csv"
         
-        #m.add_geojson(regions_geojson, layer_name="臺中市區界")
-        m.add_geojson(response, layer_name="新化斷層活動斷層地質敏感區")
-        #m.add_points_from_xy(
-            #bus_stop,
-            #x="經度",
-            #y="緯度",
-            #icon_names=["gear", "map", "leaf", "globe"],
-            #spin=True,
-            #add_legend=True,
-        #)
+        m.add_points_from_xy(
+            bus_stop,
+            x="經度",
+            y="緯度",
+            icon_names=["gear", "map", "leaf", "globe"],
+            spin=True,
+            add_legend=True,
+        )
 
 m.to_streamlit(height=700)
