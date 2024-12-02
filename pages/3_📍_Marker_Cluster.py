51727ad795a011ca8c9d2e4bb0221c9a75bd2114
b6baa <tina200205106@gmail.com>
2024-12-02 13:44:38 +0800

    Update 3_📍_Marker_Cluster.py

--- pages/3_📍_Marker_Cluster.py
+++ pages/3_📍_Marker_Cluster.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
-import geopandas as gpd
-import requests
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
@@ -19,14 +18,14 @@ st.title("Marker Cluster")
 
 with st.expander("See source code"):
     with st.echo():
-        m = leafmap.Map(center=[24.14734503954353, 120.67316364818225], zoom=14)
-        bus_stop = gpd.read_file("https://github.com/b6baa/20241127ex2/raw/refs/heads/main/%E8%87%BA%E4%B8%AD%E5%B8%82%E5%81%A5%E8%A1%8C%E6%AD%A5%E9%81%93%E8%B3%87%E6%96%991131024(%E4%BF%AE%E5%A2%9E%E5%8A%A0%E7%B6%93%E7%B7%AF%E5%BA%A6).csv.csv")
+        m = leafmap.Map(center=[24.14734503954353, 120.67316364818225], zoom=10)
+        m.add_basemap(TERRAIN)
+        data = pd.read_file("https://github.com/b6baa/20241127ex2/raw/refs/heads/main/%E8%87%BA%E4%B8%AD%E5%B8%82%E5%81%A5%E8%A1%8C%E6%AD%A5%E9%81%93%E8%B3%87%E6%96%991131024(%E4%BF%AE%E5%A2%9E%E5%8A%A0%E7%B6%93%E7%B7%AF%E5%BA%A6).csv.csv")
         
         m.add_points_from_xy(
-            bus_stop,
+            data,
             x="經度(步道大約位置)",
             y="緯度(步道大約位置)",
-            icon_names=["gear", "map", "leaf", "globe"],
             spin=True,
             add_legend=True,
         )
