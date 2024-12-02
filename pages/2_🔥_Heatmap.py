902f7104bec44415d47fb080c6827b9c23a4f75e
b6baa <tina200205106@gmail.com>
2024-12-02 14:03:32 +0800

    Update 2_🔥_Heatmap.py

--- pages/2_🔥_Heatmap.py
+++ pages/2_🔥_Heatmap.py
@@ -17,12 +17,12 @@ st.title("Heatmap")
 
 with st.expander("See source code"):
     with st.echo():
-        filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
-        m = leafmap.Map(center=[40, -100], zoom=4)
+        filepath = "https://github.com/b6baa/20241127ex2/raw/refs/heads/main/%E8%87%BA%E4%B8%AD%E5%B8%82%E5%81%A5%E8%A1%8C%E6%AD%A5%E9%81%93%E8%B3%87%E6%96%991131024(%E4%BF%AE%E5%A2%9E%E5%8A%A0%E7%B6%93%E7%B7%AF%E5%BA%A6).csv.csv"
+        m = leafmap.Map(center=[24.14734503954353, 120.67316364818225], zoom=10)
         m.add_heatmap(
             filepath,
-            latitude="latitude",
-            longitude="longitude",
+            latitude="緯度(步道大約位置)",
+            longitude"經度(步道大約位置)"=,
             value="pop_max",
             name="Heat map",
             radius=20,
