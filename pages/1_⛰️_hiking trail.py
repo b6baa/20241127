36d647d5c03a6b3163961a228d76afe4f0fb8662
b6baa <tina200205106@gmail.com>
2024-12-02 14:43:42 +0800

    Update 1_⛰️_hiking trail.py

--- pages/1_⛰️_hiking trail.py
+++ pages/1_⛰️_hiking trail.py
@@ -18,7 +18,7 @@ st.title("Marker Cluster")
 
 with st.expander("See source code"):
     with st.echo():
-        m = leafmap.Map(center=[24.14734503954353, 120.67316364818225], zoom=10)
+        m = leafmap.Map(center=[24.14734503954353, 120.67316364818225], zoom=10, minimap_control=True)
         m.add_basemap("TERRAIN")
         data = pd.read_csv("https://github.com/b6baa/20241127ex2/raw/refs/heads/main/%E8%87%BA%E4%B8%AD%E5%B8%82%E5%81%A5%E8%A1%8C%E6%AD%A5%E9%81%93%E8%B3%87%E6%96%991131024(%E4%BF%AE%E5%A2%9E%E5%8A%A0%E7%B6%93%E7%B7%AF%E5%BA%A6).csv.csv")
         
