609115254ec8959a2bc9cdb9f36f12c36eebf6b4
b6baa <tina200205106@gmail.com>
2024-12-02 14:43:31 +0800

    Update 2_🔥_Heatmap.py

--- pages/2_🔥_Heatmap.py
+++ pages/2_🔥_Heatmap.py
@@ -15,15 +15,21 @@ st.sidebar.image(logo)
 
 st.title("Heatmap")
 
+st.markdown(
+    """
+    以步道長度做熱區圖，顏色越紅代表該地區的步道越長。
+    """
+)
+
 with st.expander("See source code"):
     with st.echo():
         filepath = "https://github.com/b6baa/20241127ex2/raw/refs/heads/main/%E8%87%BA%E4%B8%AD%E5%B8%82%E5%81%A5%E8%A1%8C%E6%AD%A5%E9%81%93%E8%B3%87%E6%96%991131024(%E4%BF%AE%E5%A2%9E%E5%8A%A0%E7%B6%93%E7%B7%AF%E5%BA%A6).csv.csv"
-        m = leafmap.Map(center=[24.14734503954353, 120.67316364818225], zoom=10)
+        m = leafmap.Map(center=[24.14734503954353, 120.67316364818225], zoom=10, minimap_control=True)
         m.add_heatmap(
             filepath,
             latitude="緯度(步道大約位置)",
             longitude="經度(步道大約位置)",
-            value="pop_max",
+            value="長度公里",
             name="Heat map",
             radius=20,
         )
