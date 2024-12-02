939a8e6542511a914c763e48b4cc0ba6eb035fa7
b6baa <tina200205106@gmail.com>
2024-12-02 11:08:00 +0800

    Update 3_📍_Marker_Cluster.py

--- pages/3_📍_Marker_Cluster.py
+++ pages/3_📍_Marker_Cluster.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
-import pandas as pd
 import geopandas as gpd
 
 st.set_page_config(layout="wide")
@@ -19,24 +18,17 @@ st.title("Marker Cluster")
 
 with st.expander("See source code"):
     with st.echo():
-
-        # 初始化地圖
-        m = leafmap.Map(center=[24.1477, 120.6736], zoom=12)
-
-        # 讀取 Shapefile 並轉換為 GeoJSON
         gdf = gpd.read_file("台中市區界_TWD97-1.shp")
         regions_geojson = gdf.to_json()
+
+        m = leafmap.Map(center=[40, -100], zoom=4)
+        bus_stop = "臺中市市區公車站牌資料.csv"
+        
         m.add_geojson(regions_geojson, layer_name="臺中市區界")
-
-        # 讀取公車站牌資料
-        cities = pd.read_csv("臺中市市區公車站牌資料.csv")
-
-        # 將站牌資料加入地圖
         m.add_points_from_xy(
-            cities,
+            bus_stop,
             x="經度",
             y="緯度",
-            color_column="region",
             icon_names=["gear", "map", "leaf", "globe"],
             spin=True,
             add_legend=True,
