e24780b2f31154767ac1e074934aff3a53f5c337
b6baa <tina200205106@gmail.com>
2024-12-02 13:23:14 +0800

    Update 3_📍_Marker_Cluster.py

--- pages/3_📍_Marker_Cluster.py
+++ pages/3_📍_Marker_Cluster.py
@@ -20,7 +20,7 @@ st.title("Marker Cluster")
 with st.expander("See source code"):
     with st.echo():
         m = leafmap.Map(center=[40, -100], zoom=4)
-        bus_stop = "https://github.com/b6baa/20241127ex2/raw/refs/heads/main/%E8%87%BA%E4%B8%AD%E5%B8%82%E5%B8%82%E5%8D%80%E5%85%AC%E8%BB%8A%E7%AB%99%E7%89%8C%E8%B3%87%E6%96%99.csv"
+        bus_stop = gpd.read_file("https://github.com/b6baa/20241127ex2/raw/refs/heads/main/%E8%87%BA%E4%B8%AD%E5%B8%82%E5%B8%82%E5%8D%80%E5%85%AC%E8%BB%8A%E7%AB%99%E7%89%8C%E8%B3%87%E6%96%99.csv")
         
         m.add_points_from_xy(
             bus_stop,
